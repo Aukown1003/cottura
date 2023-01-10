817f5c671c398566d562611923bd21fac404178f
YsukeKuwano <115121908+Aukown1003@users.noreply.github.com>
2023-01-10 06:41:13 +0000

    [Add]ヘルパーメソッドの追加

--- app/controllers/public/recipes_controller.rb
+++ app/controllers/public/recipes_controller.rb
@@ -1,13 +1,29 @@
 class Public::RecipesController < ApplicationController
   def new
+    @recipe = Recipe.new
   end
 
   def index
   end
 
   def show
+    
   end
 
   def edit
   end
+  
+  private
+  def person_params
+      params.require(:recipe).permit(
+        :user_id,
+        :title,
+        :content,
+        :total_time,
+        :is_open,
+        recipe_ingredients_attributes: [:id, :content, :quantity, :_destroy],
+        recipe_steps_attributes: [:id, :direction, :image, :_destroy]
+        )
+  end
+  
 end
