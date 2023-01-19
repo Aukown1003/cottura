2f9145742c8545b4d53a021e203eb93f9bb84cee
Auk1003 <Aukown1003@users.noreply.github.com>
2023-01-19 20:33:57 +0900

    [Fix]セレクトボックス表示

--- app/models/user.rb
+++ app/models/user.rb
@@ -38,7 +38,7 @@ class User < ApplicationRecord
 
   # ゲストユーザーログインメソッド
   def self.guest
-    find_or_create_by!(name: 'guestuser' ,email: 'guest@example.com') do |user|
+    find_or_create_by!(email: 'guest@example.com') do |user|
       user.password = SecureRandom.urlsafe_base64
       user.name = "ゲストユーザー"
       user.content = "ゲストユーザーでログイン中！"
