3d868dc440ae4f39620dac49a0b0402d61e9a45d
Aukown1003 <115121908+Aukown1003@users.noreply.github.com>
2023-02-23 02:38:12 +0000

    [Update]recipeモデルテストの分類、一部テストの追加

--- spec/support/common_helper.rb
+++ spec/support/common_helper.rb
@@ -1,4 +1,9 @@
 module CommonHelpers
+  def build_recipe_with_ingredient_and_step(recipe, unit)
+    recipe.recipe_ingredients.build(attributes_for(:recipe_ingredient, unit_id: unit.id))
+    recipe.recipe_steps.build(attributes_for(:recipe_step))
+  end
+  
   def save_recipe_with_ingredient_and_step(recipe, unit)
     recipe.recipe_ingredients.build(attributes_for(:recipe_ingredient, unit_id: unit.id))
     recipe.recipe_steps.build(attributes_for(:recipe_step))
